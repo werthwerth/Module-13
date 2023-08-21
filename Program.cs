380e1b826ec9aa9f0bbf707c2e17db5a6ee45de5
werthwerth <teleperedozik@mail.ru>
2023-08-21 10:42:34 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string readText = File.ReadAllText(@"./Text1.txt");
+        var rows = readText.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+        List<string> Text = new List<string>();
+        var stopWatch = Stopwatch.StartNew();
+        foreach(string r in rows)
+        {
+            Text.Append(r);
+        }
+        Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+
+        stopWatch.Reset();
+        stopWatch.Restart();
+        LinkedList<string> Text1 = new LinkedList<string>();
+        foreach (string r in rows)
+        {
+            Text1.AddLast(r);
+        }
+        Console.WriteLine(stopWatch.Elapsed.TotalMilliseconds);
+    }
+}
